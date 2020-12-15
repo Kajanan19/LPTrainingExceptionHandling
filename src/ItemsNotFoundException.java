79ccd060f6949a2b58ddb99862b227fbfdaeb276
Kajanan19 <kajanankaj19@gmail.com>
2020-12-15 13:26:20 +0530

    CoomitOne

---
+++ src/ItemsNotFoundException.java
@@ -0,0 +1,10 @@
+
+public class ItemsNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public String toString() {
+		return("Items not found Exception");
+	}
+
+}
