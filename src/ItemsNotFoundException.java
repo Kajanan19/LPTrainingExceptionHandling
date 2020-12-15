5af7c3f46f22bfdae7e2a008ed9c432e80ca4b5b
Kajanan19 <kajanankaj19@gmail.com>
2020-12-15 13:49:54 +0530

    CommitTwo

--- src/ItemsNotFoundException.java
+++ src/ItemsNotFoundException.java
@@ -3,8 +3,8 @@ public class ItemsNotFoundException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 
-	public String toString() {
-		return("Items not found Exception");
+	public ItemsNotFoundException(String message) {
+		super(message);
 	}
 
 }
